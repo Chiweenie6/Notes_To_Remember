fff51f9f561d347169886449307025b7087ec299
chiweenie6 <113393706+Chiweenie6@users.noreply.github.com>
2023-02-01 18:25:42 -0700

    working on app.post

--- Develop/server.js
+++ Develop/server.js
@@ -13,10 +13,21 @@ app.use(express.urlencoded({ extended: true}));
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "index.html"));
-});
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+  });
+
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
+  });
 
 app.get("/api/notes", (req, res) => res.json(notesDB));
+
+
+app.post("/api/notes", (req, res) => {
+    res.json(`${req.method} note recieved.`);
+
+console.info(`${req.method} note Recieved.`);
+});
 
 app.listen(PORT, () => {
     console.log(`Begin server on port http://localhost:${PORT} 👍.`);
